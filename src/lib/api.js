b32f828e511f652a0d9b4b7fb9b13ecf5a66108a
yguhan <yguhan@gmail.com>
2018-03-07 04:35:39 +0900

    Add ref link

--- src/lib/api.js
+++ src/lib/api.js
@@ -5,6 +5,7 @@ const Waves = WavesAPI.create(WavesAPI.MAINNET_CONFIG);
 export function getTxs(address) {
     // NOTE: limit 50
     // v1 json != v2 json   
+    // SEE: https://github.com/wavesplatform/waves-api/tree/master/src/api/node
     return Waves.API.Node.v1.transactions.getList(address);
 }
 
