0fd0e08ba5bfa3c299ad9914d22960fab0516613
YUNGU HAN <yghan@postech.ac.kr>
2018-03-06 19:43:38 +0900

    add waves asset id

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,4 +1,4 @@
 export const UFIC_ASSET_ID = 'E63d9G5CTmyABNL3ztM72zi5c7PcM1PRtnR3bL9LCTCX';
 export const UFIC_WALLET_ADDRESS = '3PPWHS17wRorfaYWkthwSMkSFVd33fausvj';
 export const UFICTOKENEXPLORER_VERSION = "0.0";
-
+export const WAVES_ASSET_ID = 'WAVES';
