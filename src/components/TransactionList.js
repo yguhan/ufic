d85c07fb7ce730cbe221cebc9c7a3e7729488aad
YUNGU HAN <yghan@postech.ac.kr>
2018-03-07 02:35:52 +0900

    Move html tags to TransactionInfo component

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import * as Constants from '../constants/constants'
 import Balance from './Balance';
+import TransactionInfo from './TransactionInfo';
 import _ from 'underscore';
 
 class TransactionList extends Component {
@@ -19,25 +20,16 @@ class TransactionList extends Component {
                 <Balance />
                 
                 <div className="panel panel-default">
-                    <div className="panel-heading">거래일시 / 종류 / 입금액 / 출금액 / 보낸 주소 / 받는 주소 / tx </div>
+                    <div className="panel-heading">거래일시 / 입금액 / 출금액 / 수수료 / 보낸 주소 / 받는 주소 / 메모 / tx </div>
                     <table className="table" style={{textAlign: 'left'}}>
                         <thead>
                         <tr>
-                            <td>거래일시</td><td>종류</td><td>입금액</td><td>출금액</td><td>보낸 주소</td><td>받는 주소</td><td>tx</td>
+                            <td>거래일시</td><td>입금액</td><td>출금액</td><td>수수료</td><td>보낸 주소</td><td>받는 주소</td><td>메모</td><td>tx</td>
                         </tr>
                         </thead>
                         <tbody>
                             {txList.map((tx, i) => {
-                                return (
-                                    <tr key={i}>
-                                        <td>{tx.timestamp}</td>
-                                        <td>{tx.assetId == null ? 'WAVES' : 'UFIC'}</td>
-                                        <td>{tx.sender == Constants.UFIC_WALLET_ADDRESS ? 0 : tx.amount}</td>
-                                        <td>{tx.sender !== Constants.UFIC_WALLET_ADDRESS ? 0 : tx.amount}</td>
-                                        <td><a href={`https://wavesexplorer.com/address/${tx.sender}`}>보낸 주소</a></td>
-                                        <td><a href={`https://wavesexplorer.com/address/${tx.recipient}`}>받는 주소</a></td>
-                                        <td><a href={`https://wavesexplorer.com/tx/${tx.id}`}>tx</a></td>
-                                    </tr>);
+                                return <TransactionInfo tx={tx} key={i} />
                             })}
                         </tbody>
                     </table>
