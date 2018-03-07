e2301fc687ba079d529d9ac40f88534a5e9d40b1
yguhan <yguhan@gmail.com>
2018-03-08 03:19:01 +0900

    Applied v2 api

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react';
 import * as Constants from '../constants/constants';
 import * as API from '../lib/api';
-import _ from 'underscore';
-import { parseFormattedAssetNumber } from '../lib/common';
+import _ from 'lodash';
+import { getAssetAmount } from '../lib/common';
 
 class Balance extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            wavesBalance: 0,
-            uficBalance: 0,
+            balances: [],
             fetching: true,
         }
     }
@@ -24,29 +23,27 @@ class Balance extends Component {
             fetching: true,
         });
 
-        const wavesBalance = await API.getBalance(Constants.UFIC_WALLET_ADDRESS, Constants.WAVES_ASSET_ID);
-        const uficBalance = await API.getBalance(Constants.UFIC_WALLET_ADDRESS, Constants.UFIC_ASSET_ID);
-        
+        const balances = await API.getBalances(Constants.UFIC_WALLET_ADDRESS);
+
         this.setState({
-            wavesBalance: wavesBalance,
-            uficBalance: uficBalance,
+            balances: balances,
             fetching: false,
         });
-    }
 
+    }
+    
     render() {
-        const {wavesBalance, uficBalance, fetching} = this.state;
+        const {balances, fetching} = this.state;
 
         return (
             <div>
                 <h5>@동아리 지갑 주소: <a href={`https://wavesexplorer.com/address/${Constants.UFIC_WALLET_ADDRESS}`}>{Constants.UFIC_WALLET_ADDRESS}</a></h5>
                 <div className="alert alert-success" role="alert">
                     
-                    {!fetching && 
-                        <div>
-                            <p>Total WAVES: {parseFormattedAssetNumber(Constants.WAVES_ASSET_ID, wavesBalance.balance)}</p>
-                            <p>Total UFIC : {parseFormattedAssetNumber(uficBalance.assetId, uficBalance.balance)}</p> 
-                        </div>
+                    {!fetching &&
+                        balances.map((balance) => {
+                            return <p>Total {_.get(balance, 'asset.name')} : {getAssetAmount(balance)} </p>
+                        }) 
                     }
                 </div>
             </div>
