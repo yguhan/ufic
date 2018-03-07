cb121aafe0ce6c151b9fd7a4708132bfadd9ee3b
yguhan <yguhan@gmail.com>
2018-03-08 03:03:15 +0900

    Moved function to common/index.js + Deleted decode part

--- src/components/TransactionInfo.js
+++ src/components/TransactionInfo.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import * as Constants from '../constants/constants'
 import Balance from './Balance';
-import { parseFormattedAssetNumber, parseShortenString, decodeAttachment } from '../lib/common';
-import _ from 'underscore';
+import { parseShortenString, getAssetAmount } from '../lib/common';
+import _ from 'lodash';
 import moment from 'moment';
 import 'moment/locale/ko';
 
@@ -14,7 +14,6 @@ class TransactionInfo extends Component {
         super(props);
         this.state = {
         }
-        this.getAssetAmount = this.getAssetAmount.bind(this);
     }
 
 
@@ -26,32 +25,16 @@ class TransactionInfo extends Component {
             // TODO: 종류 없애고 1000 UFIC, 0.1 WAVES 형태로
             <tr>
                 <td>{moment(tx.timestamp).format('YYYY hh:mm:ss')}</td>
-                <td>{tx.recipient == Constants.UFIC_WALLET_ADDRESS ? this.getAssetAmount(tx.assetId, tx.amount) : 0}</td>
-                <td>{tx.sender == Constants.UFIC_WALLET_ADDRESS ? this.getAssetAmount(tx.assetId, tx.amount) : 0}</td>
-                <td>{this.getAssetAmount(null, tx.fee)}</td>
-                <td><a href={`https://wavesexplorer.com/address/${tx.sender}`}>{ parseShortenString(tx.sender) }</a></td>
-                <td><a href={`https://wavesexplorer.com/address/${tx.recipient}`}>{ parseShortenString(tx.recipient) }</a></td>
-                <td>{ decodeAttachment(tx.attachment) }</td>
-                <td><a href={`https://wavesexplorer.com/tx/${tx.id}`}>{ parseShortenString(tx.id) }</a></td>
+                <td>{tx.recipient == Constants.UFIC_WALLET_ADDRESS ? getAssetAmount(tx.amount) : 0}</td>
+                <td>{tx.sender == Constants.UFIC_WALLET_ADDRESS ? getAssetAmount(tx.amount) : 0}</td>
+                <td>{tx.sender == Constants.UFIC_WALLET_ADDRESS ? getAssetAmount(tx.fee) : 0}</td>
+                <td><a href={`${Constants.WAVESEXPLORER_URI}/address/${tx.sender}`}>{ parseShortenString(tx.sender) }</a></td>
+                <td><a href={`${Constants.WAVESEXPLORER_URI}/address/${tx.recipient}`}>{ parseShortenString(tx.recipient) }</a></td>
+                <td>{tx.attachment}</td>
+                <td><a href={`${Constants.WAVESEXPLORER_URI}/tx/${tx.id}`}>{ parseShortenString(tx.id) }</a></td>
             </tr>            
         );
     }
-
-    getAssetAmount(assetId, amount) {
-        switch(assetId) {
-            case null: {
-                return `${parseFormattedAssetNumber(Constants.WAVES_ASSET_ID, amount)} WAVES`;
-            }
-            case Constants.UFIC_ASSET_ID : {
-                return `${parseFormattedAssetNumber(assetId, amount)} UFIC`;
-            }
-            default : {
-                return `${amount} ?`;
-            } 
-        
-        }
-    }
-
 }
 
 export default TransactionInfo;
