389d52d74a743ced1ef1dd596d72f2fc2a4c489b
yguhan <yguhan@gmail.com>
2018-03-08 02:51:03 +0900

    Updated constants

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -5,3 +5,8 @@ export const UFIC_ASSET_DECIMALS = 6;
 
 export const WAVES_ASSET_ID = 'WAVES';
 export const WAVES_ASSET_DECIMALS = 8;
+
+export const WAVESEXPLORER_URI = 'https://wavesexplorer.com';
+
+export const UFIC_REGISTER_ADDRESS = '3P7emxzmXoFk72ZMBx63Gu1QrCoSinFGksf';
+export const UFIC_VOTE_ADDRESS = '3PJE1GFtRfnoefmMHHuXT5QiR4akdbNPaGZ';
