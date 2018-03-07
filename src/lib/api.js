eded6cb4d5e184269ec151d9b00f8fba517e1f62
yguhan <yguhan@gmail.com>
2018-03-08 03:00:50 +0900

    Changed getTxs and getBalances using v2

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,19 +1,25 @@
 import * as WavesAPI from 'waves-api';
 import * as Constants from '../constants/constants';
+import _ from 'lodash';
 const Waves = WavesAPI.create(WavesAPI.MAINNET_CONFIG);
 
 export function getTxs(address) {
-    // NOTE: limit 50
-    // v1 json != v2 json   
-    // SEE: https://github.com/wavesplatform/waves-api/tree/master/src/api/node
-    return Waves.API.Node.v1.transactions.getList(address);
+    // SEE: https://github.com/wavesplatform/waves-api/blob/master/src/api/node/v2/addresses.ts
+    return Waves.API.Node.v2.addresses.transactions(address);
 }
 
-export function getBalance(address, assetId) {
-    return Waves.API.Node.v1.assets.balance(address, assetId);
+export function getBalances(address) {
+    return Waves.API.Node.v2.addresses.balances(address);
+}
+
+// NOTE: Waves.API.Node.v2.addresses.balance(address, assetId) always return 0 amount
+export function getBalance(address, assetId=Constants.WAVES_ASSET_ID) {
+    return Waves.API.Node.v2.addresses.balances(address).then((balances) => {
+        return _.find(balances, (balance) => _.get(balance, 'asset.id') == assetId);
+    })
 }
 
 export function getAssetDistribution(assetId) {
-    // Map
+    // return type is Map
     return Waves.API.Node.v1.assets.distribution(assetId);
 }
