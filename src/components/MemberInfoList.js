e7df821a7cec717ec5c8ab74c4c3f24246623ecf
yguhan <yguhan@gmail.com>
2018-03-08 03:17:30 +0900

    Applied props.memberInfoList

--- src/components/MemberInfoList.js
+++ src/components/MemberInfoList.js
@@ -1,36 +1,63 @@
 import React, { Component } from 'react';
-import _ from 'underscore';
+import * as Constants from '../constants/constants'
+import * as API from '../lib/api';
+import _ from 'lodash';
+import { parseShortenString } from '../lib/common';
 
 class MemberInfoList extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-        }
     }
 
     render() {
         return (
             <div className="container" style={{maxWidth: 1000}}>
-                
+               
                 <div className="panel panel-default">
-                    <div className="panel-heading">주소 / 보유 UFIC</div>
+                    <div className="panel-heading">동아리원 정보</div>
                     <table className="table" style={{textAlign: 'left'}}>
                         <thead>
                         <tr>
-                            <td>주소</td><td>보유 UFIC</td>
+                            {_.keys(this.props.memberInfoList[0]).map((key, i) => {
+                                return (
+                                <td>{key}</td>
+                                );
+                            })}
                         </tr>
                         </thead>
                         <tbody>
-                            {_.keys(this.props.uficHoldersMap).map((key, i) => {
+                            {/* [{}, {}] */}
+                            {this.props.memberInfoList.map((memberInfo, i) => {
+                                // // {} -> <tr> ... <tr>
                                 return (
-                                <tr key={i}>
-                                    <td>{key}</td>
-                                    <td>{this.props.uficHoldersMap[key]}</td>
-                                </tr>);
-                            })}  
+                                    <tr>
+                                        {_.keys(memberInfo).map((key, i) => {
+                                            switch (key) {
+                                                case '주소': {
+                                                    return (
+                                                        <td><a href={`${Constants.WAVESEXPLORER_URI}/address/${memberInfo[key]}`}>{parseShortenString(memberInfo[key])}</a></td>
+                                                    )
+                                                }
+                                                case 'tx': {
+                                                    return (
+                                                        <td><a href={`${Constants.WAVESEXPLORER_URI}/tx/${memberInfo[key]}`}>{parseShortenString(memberInfo[key])}</a></td>
+                                                    )
+                                                }
+                                                default : {
+                                                    return (
+                                                        <td>{memberInfo[key]}</td>
+                                                    )
+                                                }
+
+                                            }
+                                        })}
+                                    </tr>
+                                )
+                            })}
                         </tbody>
                     </table>
                 </div>
+            
             </div>
         );
     }
