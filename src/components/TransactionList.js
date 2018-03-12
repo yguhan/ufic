3fd82e29751526c1da87df1c261d6332d2686567
yguhan <yguhan@gmail.com>
2018-03-13 00:18:45 +0900

    Indented tags

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -21,18 +21,18 @@ class TransactionList extends Component {
                 
                 <div className="panel panel-default">
                     <div className="panel-heading">거래 내역</div>
-                    <table className="table" style={{textAlign: 'left'}}>
-                        <thead>
-                        <tr>
-                            <td>거래일시</td><td>입금액</td><td>출금액</td><td>수수료</td><td>보낸 주소</td><td>받는 주소</td><td>메모</td><td>tx</td>
-                        </tr>
-                        </thead>
-                        <tbody>
-                            {txList.map((tx, i) => {
-                                return <TransactionInfo tx={tx} key={i} />
-                            })}
-                        </tbody>
-                    </table>
+                        <table className="table" style={{textAlign: 'left'}}>
+                            <thead>
+                                <tr>
+                                    <td>거래일시</td><td>입금액</td><td>출금액</td><td>수수료</td><td>보낸 주소</td><td>받는 주소</td><td>메모</td><td>tx</td>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {txList.map((tx, i) => {
+                                    return <TransactionInfo tx={tx} key={i} />
+                                })}
+                            </tbody>
+                        </table>
                 </div>
             </div>
         );
