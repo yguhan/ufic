ba05db94549c1782264a3b4cc8a4dfa9d4be0f63
yguhan <yguhan@gmail.com>
2018-04-21 23:31:36 +0900

    Changed to use return value of getV1Txs

--- src/components/TransactionInfo.js
+++ src/components/TransactionInfo.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import * as Constants from '../constants/constants'
 import Balance from './Balance';
-import { parseShortenString, getAssetAmountWithName } from '../lib/common';
+import { parseShortenString, decodeAttachment, getAssetAmountWithNameById } from '../lib/common';
 import _ from 'lodash';
 import moment from 'moment';
 import 'moment/locale/ko';
@@ -24,15 +24,16 @@ class TransactionInfo extends Component {
             // 거래일시 / 입금액 / 출금액 / 수수료 / 보낸 주소 / 받는 주소 / 메모 / tx         
             <tr>
                 <td>{moment(tx.timestamp).format('YYYY/MM/DD hh:mm:ss')}</td>
-                <td>{tx.recipient == Constants.UFIC_WALLET_ADDRESS ? getAssetAmountWithName(tx.amount) : 0}</td>
-                <td>{tx.sender == Constants.UFIC_WALLET_ADDRESS ? getAssetAmountWithName(tx.amount) : 0}</td>
-                <td>{tx.sender == Constants.UFIC_WALLET_ADDRESS ? getAssetAmountWithName(tx.fee) : 0}</td>
+                <td>{tx.recipient == Constants.UFIC_WALLET_ADDRESS ? getAssetAmountWithNameById(tx.amount, tx.assetId) : 0}</td>
+                <td>{tx.sender == Constants.UFIC_WALLET_ADDRESS ? getAssetAmountWithNameById(tx.amount, tx.assetId) : 0}</td>
+                <td>{tx.sender == Constants.UFIC_WALLET_ADDRESS ? getAssetAmountWithNameById(tx.fee, Constants.WAVES_ASSET_ID) : 0}</td>
                 <td><a href={`${Constants.WAVESEXPLORER_URI}/address/${tx.sender}`}>{ parseShortenString(tx.sender) }</a></td>
                 <td><a href={`${Constants.WAVESEXPLORER_URI}/address/${tx.recipient}`}>{ parseShortenString(tx.recipient) }</a></td>
-                <td>{tx.attachment}</td>
+                <td>{ decodeAttachment(tx.attachment) }</td>
                 <td><a href={`${Constants.WAVESEXPLORER_URI}/tx/${tx.id}`}>{ parseShortenString(tx.id) }</a></td>
             </tr>            
         );
+        
     }
 }
 
